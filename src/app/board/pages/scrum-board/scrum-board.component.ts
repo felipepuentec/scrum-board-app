9aca8d8b18248a838aed8cc4acc57d9a6eb9ed59
felipepuentecap <felipe.puente@capmation.com>
2024-04-29 12:15:40 -0500

    Started funcionality

--- src/app/board/pages/scrum-board/scrum-board.component.ts
+++ src/app/board/pages/scrum-board/scrum-board.component.ts
@@ -4,10 +4,18 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { AppBoardService } from '../../service/app-board.service';
 import { Column } from '../../interfaces/column.interface';
 
+import {
+  CdkDragDrop,
+  CdkDropList,
+  moveItemInArray,
+  transferArrayItem,
+} from '@angular/cdk/drag-drop';
+import { CardProps } from '../../interfaces/card-props.interface';
+
 @Component({
   selector: 'app-scrum-board',
   standalone: true,
-  imports: [StateColumnComponent, MatToolbarModule],
+  imports: [StateColumnComponent, MatToolbarModule, CdkDropList],
   templateUrl: './scrum-board.component.html',
   styleUrl: './scrum-board.component.css',
 })
@@ -18,5 +26,37 @@ export class ScrumBoardComponent {
 
   get columnsContent(): Column[] {
     return this.appBoardService.mockColumns;
+  }
+  get toDoList(): CardProps[] {
+    return this.appBoardService.toDoList.content;
+  }
+  get inProgressList(): CardProps[] {
+    return this.appBoardService.inProgressList.content;
+  }
+  get doneList(): CardProps[] {
+    return this.appBoardService.doneList.content;
+  }
+
+  drop(event: CdkDragDrop<CardProps[]>): void {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex
+      );
+    } else {
+      const transferredItem = event.previousContainer.data[event.previousIndex];
+
+      // Update status of the transferred card
+      transferredItem.status = event.container.data[0].status;
+
+      // Update status of the transferred card
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex
+      );
+    }
   }
 }
